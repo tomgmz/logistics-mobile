a8f668e11c6de1c99c86eaa87afaae613f0fbdca
Tom <tomervingmz@gmail.com>
2026-02-21 21:53:15 +0800

    first ccommit

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "../global.css";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack/>
+    </SafeAreaProvider>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
-import "./global.css"
-import { Text, View } from "react-native";
- 
-export default function App() {
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
